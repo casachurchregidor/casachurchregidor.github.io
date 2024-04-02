c2bc8c892bfd0d08f693588b07be3ee114543c47
casachurchregidor <163894558+casachurchregidor@users.noreply.github.com>
2024-04-02 10:00:32 -0500

    V 1.0.4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,54 @@
+let front = document.querySelector('.face-front');
+let back = document.querySelector('.face-back');
+let flip = document.querySelector('.book-content');
+let uno = document.querySelectorAll('.book');
+let portada = document.querySelectorAll('#portada');
+
+let contZindex = 2;
+let customZindex = 1;
+
+for (let i = 0; i < uno.length; i++) {
+	uno[i].style.zIndex = customZindex;
+	customZindex--;
+
+	uno[i].addEventListener('click', function(e){
+
+		var tgt = e.target;
+		var unoThis = this;
+
+		unoThis.style.zIndex = contZindex;
+		contZindex++;
+
+		if (tgt.getAttribute('class') == 'face-front') {
+			unoThis.style.zIndex = contZindex;
+			contZindex +=20;
+			setTimeout(function(){
+				unoThis.style.transform = 'rotateY(-180deg)';
+			}, 500);
+		}
+		if (tgt.getAttribute("class") == 'face-back') {
+			unoThis.style.zIndex = contZindex;
+			contZindex +=20;
+
+			setTimeout(function(){
+				unoThis.style.transform = 'rotateY(0deg)';
+			}, 500);
+		}
+
+		if (tgt.getAttribute('id') == 'portada') {
+			flip.classList.remove("trnsf-reset");
+			flip.classList.add("trnsf");
+		}
+		if (tgt.getAttribute('id') == 'trsf') {
+			flip.classList.remove("trnsf");
+			flip.classList.add("trnsf-reset");
+		}
+
+	});
+}
+ 
+
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
@@ -11,3 +62,4 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
